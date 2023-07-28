584ebd25b79efbb0cc486823fbf5ccaf6cf79b0e
Vitaliy Sulimenko <prevetal@gmail.com>
2023-07-28 15:57:19 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,490 @@
+// Ширина окна для ресайза
+WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+BODY = document.getElementsByTagName('body')[0]
+OVERLAY = document.querySelector('.overlay')
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	// Основной слайдер на главной
+	let mainSlider = document.querySelector('.first_section .slider .swiper')
+
+	if (mainSlider) {
+		new Swiper('.first_section .slider .swiper', {
+			loop: true,
+			speed: 750,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 0,
+			slidesPerView: 1,
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			},
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: false,
+			}
+		})
+	}
+
+
+	// Карусель шагов
+	const stepsSliders = [],
+		steps = document.querySelectorAll('.steps .swiper')
+
+	steps.forEach(function (el, i) {
+		el.classList.add('steps_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			},
+			autoHeight: true,
+			spaceBetween: 40,
+			slidesPerView: 1
+		}
+
+		stepsSliders.push(new Swiper('.steps_s' + i, options))
+	})
+
+
+	// Карусель проектов
+	const projectsSliders = [],
+		projects = document.querySelectorAll('.projects .swiper')
+
+	projects.forEach(function (el, i) {
+		el.classList.add('projects_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			},
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			breakpoints: {
+				0: {
+					spaceBetween: 10,
+					slidesPerView: 1
+				},
+				768: {
+					spaceBetween: 24,
+					slidesPerView: 3
+				},
+				1024: {
+					spaceBetween: 20,
+					slidesPerView: el.classList.contains('big') ? 2 : 3
+				}
+			}
+		}
+
+		projectsSliders.push(new Swiper('.projects_s' + i, options))
+	})
+
+
+	// Сдайдер изображений в тексте
+	const textBlockSliders = [],
+		textSliders = document.querySelectorAll('.text_block .slider .swiper')
+
+	textSliders.forEach(function (el, i) {
+		el.classList.add('text_block_s' + i)
+
+		let options = {
+			loop: true,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			},
+			spaceBetween: 24,
+			slidesPerView: 1
+		}
+
+		textBlockSliders.push(new Swiper('.text_block_s' + i, options))
+	})
+
+
+	// Страница проекта
+	let projectSlider = document.querySelector('.project_info .swiper')
+
+	if (projectSlider) {
+		new Swiper('.project_info .swiper', {
+			loop: true,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 24,
+			slidesPerView: 1,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			}
+		})
+	}
+
+
+	// Fancybox
+	Fancybox.defaults.autoFocus = false
+	Fancybox.defaults.trapFocus = false
+	Fancybox.defaults.dragToClose = false
+	Fancybox.defaults.placeFocusBack = false
+	Fancybox.defaults.l10n = {
+		CLOSE: "Закрыть",
+		NEXT: "Следующий",
+		PREV: "Предыдущий",
+		MODAL: "Вы можете закрыть это модальное окно нажав клавишу ESC"
+	}
+
+	Fancybox.defaults.template = {
+		closeButton: '<svg><use xlink:href="images/sprite.svg#ic_close"></use></svg>',
+		spinner: '<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="25 25 50 50" tabindex="-1"><circle cx="50" cy="50" r="20"/></svg>',
+		main: null
+	}
+
+
+	// Всплывающие окна
+	const modalBtns = document.querySelectorAll('.modal_btn')
+
+	if (modalBtns) {
+		modalBtns.forEach(el => {
+			el.addEventListener('click', e => {
+				e.preventDefault()
+
+				Fancybox.close()
+
+				Fancybox.show([{
+					src: document.getElementById(el.getAttribute('data-modal')),
+					type: 'inline'
+				}])
+			})
+		})
+	}
+
+
+	// Увеличение картинки
+	Fancybox.bind('.fancy_img', {
+		Image: {
+			zoom: false,
+		},
+		Thumbs: {
+			autoStart: false,
+		}
+	})
+
+
+	// Аккордион
+	$('body').on('click', '.accordion .accordion_item .head', function(e) {
+		e.preventDefault()
+
+		const $item = $(this).closest('.accordion_item'),
+			$accordion = $(this).closest('.accordion')
+
+		if ($item.hasClass('active')) {
+			$item.removeClass('active').find('.data').slideUp(300)
+		} else {
+			$accordion.find('.accordion_item').removeClass('active')
+			$accordion.find('.data').slideUp(300)
+
+			$item.addClass('active').find('.data').slideDown(300)
+		}
+	})
+
+
+	// Мини всплывающие окна
+	$('.mini_modal_btn').click(function(e) {
+		e.preventDefault()
+
+		const modalId = $(this).data('modal-id')
+
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active')
+			$('.mini_modal').removeClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'default')
+		} else {
+			$('.mini_modal_btn').removeClass('active')
+			$(this).addClass('active')
+
+			$('.mini_modal').removeClass('active')
+			$(modalId).addClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'pointer')
+		}
+	})
+
+	// Закрываем всплывашку при клике за её пределами
+	$(document).click(e => {
+		if ($(e.target).closest('.modal_cont').length === 0) {
+			$('.mini_modal, .mini_modal_btn').removeClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'default')
+		}
+	})
+
+	// Закрываем всплывашку при клике на крестик во всплывашке
+	$('.mini_modal .close_btn').click(e => {
+		e.preventDefault()
+
+		$('.mini_modal, .mini_modal_btn').removeClass('active')
+
+		if (is_touch_device()) $('body').css('cursor', 'default')
+	})
+
+
+	// Моб. меню
+	$('aside .mob_menu_btn').click((e) => {
+		e.preventDefault()
+
+		$('aside .mob_menu_btn').toggleClass('active')
+		$('aside .menu, aside .bottom').toggleClass('show')
+	})
+
+
+	// Маска ввода
+	const phoneInputs = document.querySelectorAll('input[type=tel]')
+
+	if (phoneInputs) {
+		phoneInputs.forEach(el => {
+			IMask(el, {
+				mask: '+{7} (000) 000-00-00',
+				lazy: true
+			})
+		})
+	}
+
+
+	// Фокус при клике на название поля
+	const formLabels = document.querySelectorAll('form .label')
+
+	if (formLabels) {
+		formLabels.forEach(el => {
+			el.addEventListener('click', e => {
+				e.preventDefault()
+
+				el.closest('.line').querySelector('.input, textarea').focus()
+			})
+		})
+	}
+
+
+	// Выбор файла
+	const fileInputs = document.querySelectorAll('form input[type=file]')
+
+	if (fileInputs) {
+		fileInputs.forEach(el => {
+			el.addEventListener('change', () => el.closest('.file').querySelector('label .path').innerText = el.value)
+		})
+	}
+
+
+	if (is_touch_device()) {
+		const subMenus = document.querySelectorAll('header .menu .sub_menu')
+
+		// Подменю на тач скрине
+		$('header .menu_item > a.sub_link').addClass('touch_link')
+
+		$('header .menu_item > a.sub_link').click(function (e) {
+			const $dropdown = $(this).next()
+
+			if ($dropdown.css('visibility') === 'hidden') {
+				e.preventDefault()
+
+				subMenus.forEach(el => el.classList.remove('show'))
+				$dropdown.addClass('show')
+
+				BODY.style = 'cursor: pointer;'
+			}
+		})
+
+		// Закрываем под. меню при клике за её пределами
+		document.addEventListener('click', e => {
+			if ($(e.target).closest('.menu').length === 0) {
+				subMenus.forEach(el => el.classList.remove('show'))
+
+				BODY.style = 'cursor: default;'
+			}
+		})
+	}
+
+
+	// Отправка форм
+	$('.custom_submit').submit(function(e) {
+		e.preventDefault()
+
+		Fancybox.close()
+
+		Fancybox.show([{
+			src: '#success_modal',
+			type: 'inline'
+		}])
+	})
+
+
+	// Кастомный select
+	const selects = document.querySelectorAll('select')
+
+	if (selects) {
+		selects.forEach(el => NiceSelect.bind(el))
+	}
+
+
+	// Квиз
+	var currentStep = 1,
+		totalSteps = 5
+
+	$('.quiz .head .count .total').text(totalSteps)
+
+
+	$heightRange = $('.quiz #height_range').ionRangeSlider({
+		min: 120,
+		max: 210,
+		from: 168,
+		step: 1,
+		postfix: ' см'
+	}).data("ionRangeSlider")
+
+
+	$('.quiz .next_btn').click(function (e) {
+		e.preventDefault()
+
+		currentStep++
+
+		$('.quiz .step').hide()
+		$('.quiz .step' + currentStep).fadeIn(500)
+
+		$('.quiz .progress .count .current').text(currentStep)
+		$('.quiz .progress .progress_bar div').width(currentStep / totalSteps * 100 + '%')
+
+		currentStep > 1
+			? $('.quiz .prev_btn').removeClass('disabled')
+			: $('.quiz .prev_btn').addClass('disabled')
+
+		if(currentStep == totalSteps) {
+			$('.quiz .head').hide()
+			$('.quiz .next_btn').addClass('disabled')
+		} else {
+			$('.quiz .head').show()
+			$('.quiz .next_btn').removeClass('disabled')
+		}
+	})
+
+
+	$('.quiz .prev_btn').click(function (e) {
+		e.preventDefault()
+
+		currentStep = currentStep - 1
+
+		$('.quiz .step').hide()
+		$('.quiz .step' + currentStep).fadeIn(500)
+
+		$('.quiz .progress .count .current').text(currentStep)
+		$('.quiz .progress .progress_bar div').width(currentStep / totalSteps * 100 + '%')
+
+		currentStep > 1
+			? $('.quiz .prev_btn').removeClass('disabled')
+			: $('.quiz .prev_btn').addClass('disabled')
+
+		if(currentStep == totalSteps) {
+			$('.quiz .head').hide()
+			$('.quiz .next_btn').addClass('disabled')
+		} else {
+			$('.quiz .head').show()
+			$('.quiz .next_btn').removeClass('disabled')
+		}
+	})
+})
+
+
+
+window.addEventListener('resize', function () {
+	WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+
+	let windowW = window.outerWidth
+
+	if (typeof WW !== 'undefined' && WW != windowW) {
+		// Перезапись ширины окна
+		WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+
+
+		// Моб. версия
+		if (!fakeResize) {
+			fakeResize = true
+			fakeResize2 = false
+
+			document.getElementsByTagName('meta')['viewport'].content = 'width=device-width, initial-scale=1, maximum-scale=1'
+		}
+
+		if (!fakeResize2) {
+			fakeResize2 = true
+
+			if (windowW < 390) document.getElementsByTagName('meta')['viewport'].content = 'width=390, user-scalable=no'
+		} else {
+			fakeResize = false
+			fakeResize2 = true
+		}
+	}
+})
